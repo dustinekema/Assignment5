519012192dd225d8538d77f6f0d17483a3379525
Dustin <Dustin@10.203.10.126>
2017-03-19 22:13:13 -0400

    Checking in Hello

---
+++ Cs1120_Assignment5_3-30-2017_DustinEkema/src/edu/wmich/cs1120/la5/Assignment5.java
@@ -0,0 +1,10 @@
+package edu.wmich.cs1120.la5;
+
+public class Assignment5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello");
+	}
+
+}
