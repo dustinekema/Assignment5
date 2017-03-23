ff20189e223be407bb86ed2a9f071d5fb284d57e
Ricardo <Ricardo@wireless144-072.wireless.wmich.edu>
2017-03-23 17:18:03 -0400

    Created interfaces

--- Cs1120_Assignment5_3-30-2017_DustinEkema/src/edu/wmich/cs1120/la5/Assignment5.java
+++ Cs1120_Assignment5_3-30-2017_DustinEkema/src/edu/wmich/cs1120/la5/Assignment5.java
@@ -1,13 +1,13 @@
 package edu.wmich.cs1120.la5;
 
 import edu.wmich.cs1120.la5.gui.MainWindow;
+import edu.wmich.cs1120.la5.TerrainScanner;
 
 public class Assignment5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello");
-		System.out.println("Hi");
+		MainWindow.startGUI();
 	}
 
 }
--- Cs1120_Assignment5_3-30-2017_DustinEkema/src/edu/wmich/cs1120/la5/IArea.java
+++ Cs1120_Assignment5_3-30-2017_DustinEkema/src/edu/wmich/cs1120/la5/IArea.java
@@ -1,5 +1,10 @@
 package edu.wmich.cs1120.la5;
 
 public interface IArea {
-
+	double getBasicEnergyCost();
+	void setBasicEnergyCost(double basicEnergyCost);
+	double getElevation();
+	void setElevation(double elevation);
+	double getRadiation();
+	void setRadiation(double radiation);
 }
