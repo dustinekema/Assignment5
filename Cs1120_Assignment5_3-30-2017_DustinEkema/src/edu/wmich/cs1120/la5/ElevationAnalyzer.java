b85cb0204d3e974c9ddd88f53469b631ff1df77c
Ricardo <Ricardo@resnet215-150.wireless.wmich.edu>
2017-03-29 11:46:27 -0400

    Work done on analyzer classes

--- Cs1120_Assignment5_3-30-2017_DustinEkema/src/edu/wmich/cs1120/la5/ElevationAnalyzer.java
+++ Cs1120_Assignment5_3-30-2017_DustinEkema/src/edu/wmich/cs1120/la5/ElevationAnalyzer.java
@@ -11,33 +11,33 @@ import java.util.ArrayList;
  *
  */
 public class ElevationAnalyzer implements IRover {
-
+	private ArrayList<IArea> path;
+	private String analysis;
+	
 	/**
-	 * This method will set a required method for the Rover class. This will return this 
-	 * class' ArrayList<Area> whenever it is called.
+	 * This will return this class' ArrayList<Area> whenever it is called.
 	 * @return ArrayList<Area> this is the ArrayList<Area> that will be returned.
 	 */
 	@Override
 	public ArrayList<IArea> getPath() {
 		// TODO Auto-generated method stub
-		return null;
+		return path;
 	}
 
 	/**
-	 * This method will set a required method for the Rover class. This will set this class' 
-	 * ArrayList<Area> to an area array of size specified by the parameters sent in.
+	 * This will set this class' ArrayList<Area> to an area array of 
+	 * size specified by the parameters sent in.
 	 * @param startRow This is the starting row for the Rover.
 	 * @param startCol This is the starting column for the Rover.
 	 */
 	@Override
 	public void setPath(ArrayList<IArea> path) {
 		// TODO Auto-generated method stub
-		
+		this.path = path;
 	}
 
 	/**
-	 * This method will set a required method for the Rover class. This will get the result of 
-	 * the analysis according to the type of the analyzer.
+	 * This will get the result of the analysis according to the type of the analyzer.
 	 */
 	@Override
 	public void analyzePath() {
@@ -46,30 +46,28 @@ public class ElevationAnalyzer implements IRover {
 	}
 
 	/**
-	 * This method will set a required method for the Rover class. This will return the name of 
-	 * the analyzer.
+	 * This will return the name of the analyzer.
 	 * @return String this is the analyzer that is returned.
 	 */
 	@Override
 	public String getAnalysis() {
 		// TODO Auto-generated method stub
-		return null;
+		return analysis;
 	}
 
 	/**
-	 * This method will set a required method for the Rover class. This will set the name of the 
-	 * analyzer.
-	 * @param analysis this is the name that the analyzer is set to.
+	 * This will set the analyzer to the parameter that is sent in.
+	 * @param analysis this is the string that the analysis is set to.
 	 */
 	@Override
 	public void setAnalysis(String analysis) {
 		// TODO Auto-generated method stub
-		
+		this.analysis = analysis;
 	}
 	
 	/**
-	 * This method will set a required method for the Rover class. This will return a String.
-	 * @return String this is the string that is returned.
+	 * This will return a String.
+	 * @return String This is the string that is returned.
 	 */
 	@Override
 	public String toString() {
--- Cs1120_Assignment5_3-30-2017_DustinEkema/src/edu/wmich/cs1120/la5/EnergyAnalyzer.java
+++ Cs1120_Assignment5_3-30-2017_DustinEkema/src/edu/wmich/cs1120/la5/EnergyAnalyzer.java
@@ -11,59 +11,69 @@ import java.util.ArrayList;
  *
  */
 public class EnergyAnalyzer implements IRover {
-
+	private ArrayList<IArea> path;
+	private String analysis;
+	
 	/**
-	 * This method will set a required method for the Rover class. This will return this 
-	 * class' ArrayList<Area> whenever it is called.
+	 * This will return this class' ArrayList<Area> whenever it is called.
 	 * @return ArrayList<Area> this is the ArrayList<Area> that will be returned.
 	 */
 	@Override
 	public ArrayList<IArea> getPath() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.path;
 	}
 
 	/**
-	 * This method will set a required method for the Rover class. This will set this class' 
-	 * ArrayList<Area> to an area array of size specified by the parameters sent in.
+	 * This will set this class' ArrayList<Area> to an area array of 
+	 * size specified by the parameters sent in.
 	 * @param startRow This is the starting row for the Rover.
 	 * @param startCol This is the starting column for the Rover.
 	 */
 	@Override
 	public void setPath(ArrayList<IArea> path) {
 		// TODO Auto-generated method stub
-		
+		this.path = path;
 	}
 
 	/**
-	 * This method will set a required method for the Rover class. This will get the result of 
-	 * the analysis according to the type of the analyzer.
+	 * This will get the result of the analysis according to the type of the analyzer.
 	 */
 	@Override
 	public void analyzePath() {
 		// TODO Auto-generated method stub
-		
+		double pathEnergy = 0;
+		for (int i = 0; i < path.size(); i++) {
+			pathEnergy += getPath().get(i).calcConsumedEnergy();
+		}
 	}
 
 	/**
-	 * This method will set a required method for the Rover class. This will return the name of 
-	 * the analyzer.
+	 * This will return the name of the analyzer.
 	 * @return String this is the analyzer that is returned.
 	 */
 	@Override
 	public String getAnalysis() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.analysis;
 	}
 
 	/**
-	 * This method will set a required method for the Rover class. This will return a String.
-	 * @return String this is the string that is returned.
+	 * This will set the analyzer to the parameter that is sent in.
+	 * @param analysis this is the string that the analysis is set to.
 	 */
 	@Override
 	public void setAnalysis(String analysis) {
 		// TODO Auto-generated method stub
-		
+		this.analysis = analysis;
 	}
 
+	/**
+	 * This will return a string.
+	 * @return String This is the string that is returned.
+	 */
+	@Override
+	public String toString() {
+		return null;
+	}
 }
