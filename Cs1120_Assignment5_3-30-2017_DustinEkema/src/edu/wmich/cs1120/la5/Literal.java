ae0d33b72c80c401a708b6f569cf45d148c28bd9
Dustin <Dustin@10.80.140.135>
2017-03-30 13:00:54 -0400

    made the getter and setter for Literal

--- Cs1120_Assignment5_3-30-2017_DustinEkema/src/edu/wmich/cs1120/la5/Literal.java
+++ Cs1120_Assignment5_3-30-2017_DustinEkema/src/edu/wmich/cs1120/la5/Literal.java
@@ -1,11 +1,15 @@
 package edu.wmich.cs1120.la5;
 
 public class Literal implements IExpression {
-
+private Integer value;
 	@Override
 	public Integer getValue() {
 		// TODO Auto-generated method stub
-		return null;
+		return value;
 	}
-
+	public void setValue(Integer value) {
+		// TODO Auto-generated method stub
+		this.value =value;
+	}
+	
 }
