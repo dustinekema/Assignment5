fa3bb324bca388fa4a003d6f7a44bee92971b0ec
Dustin <Dustin@10.203.10.126>
2017-03-29 20:24:26 -0400

    Started the MapCreatorFromDat and set/ get scanner

--- Cs1120_Assignment5_3-30-2017_DustinEkema/src/edu/wmich/cs1120/la5/MapCreatorFromDat.java
+++ Cs1120_Assignment5_3-30-2017_DustinEkema/src/edu/wmich/cs1120/la5/MapCreatorFromDat.java
@@ -1,6 +1,7 @@
 package edu.wmich.cs1120.la5;
 
 import java.io.IOException;
+import java.io.*;
 
 /**
  * This is the MapCreatorFromDat class, which implements the IMapCreator 
@@ -12,25 +13,51 @@ import java.io.IOException;
  *
  */
 public class MapCreatorFromDat implements IMapCreator {
-
+	private TerrainScanner scanner = new TerrainScanner();
+	private Area[][] area = new Area[10][10];
 	/**
 	 * This method reads the input file (.dat file).
 	 */
 	@Override
 	public void scanTerrain(String fileName, int threshold) throws IOException {
-		// TODO Auto-generated method stub
+		int i=0;
+		RandomAccessFile randomFile= new RandomAccessFile(fileName,"r");
+		String[] field = new String[3];
+		double basicEnergyCost = Double.parseDouble(field[0]);
+		double elevation = Double.parseDouble(field[1]);
+		double radiation = Double.parseDouble(field[2]);
+		//long x;
+		while (1<2) { // FIX THIS 
+			for (int row = 0; row < area.length; row++) {
+				for (int col = 0; col < area[0].length; col++) {
+				//	String aLine = file.nextLine();
+				//	field = aLine.split(" ");
+
+		if (radiation >= .5) {
+			Area highArea = new HighArea(basicEnergyCost, elevation, radiation);
+			area[row][col] = highArea;
+		} else if ((radiation < .5) && (elevation > (threshold * .5))) {
+			Area highArea = new HighArea(basicEnergyCost, elevation, radiation);
+			area[row][col] = highArea;
+		} else {
+			Area lowArea = new LowArea(basicEnergyCost, elevation, radiation);
+			area[row][col] = lowArea;
+		}
+				}
+			}
+		}
 		
 	}
 
 	@Override
 	public TerrainScanner getScanner() {
 		// TODO Auto-generated method stub
-		return null;
+		return scanner;
 	}
 
 	@Override
 	public void setScanner(TerrainScanner scanner) {
-		// TODO Auto-generated method stub
+		this.scanner = scanner;
 		
 	}
 
--- Cs1120_Assignment5_3-30-2017_DustinEkema/src/edu/wmich/cs1120/la5/Subtraction.java
+++ Cs1120_Assignment5_3-30-2017_DustinEkema/src/edu/wmich/cs1120/la5/Subtraction.java
@@ -4,7 +4,7 @@ public class Subtraction implements IOperation {
 
 	@Override
 	public Integer perform(IExpression left, IExpression right) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
