00880235f0ed01e81327b003a2a4c7c8c9bb0bed
Dustin <Dustin@10.80.140.135>
2017-03-30 13:28:05 -0400

    Added getters and setters to BinaryExpression and ExpressionFactory

--- Cs1120_Assignment5_3-30-2017_DustinEkema/src/edu/wmich/cs1120/la5/BinaryExpression.java
+++ Cs1120_Assignment5_3-30-2017_DustinEkema/src/edu/wmich/cs1120/la5/BinaryExpression.java
@@ -1,11 +1,14 @@
 package edu.wmich.cs1120.la5;
 
 public class BinaryExpression implements IExpression {
-
+private Integer value;
 	@Override
 	public Integer getValue() {
 		// TODO Auto-generated method stub
-		return null;
+		return value;
 	}
-
+	public void setValue(Integer value) {
+		// TODO Auto-generated method stub
+		this.value = value;
+	}
 }
